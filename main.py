32eec088e6970c09c6b0ebae863466ccbfa2e6af
ForCornCobbsandKnobs <nicholasscottfuller2077@gmail.com>
2025-09-08 20:25:57 -0400

    Updateables and Drawables. Moving triangle on the screen.

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-import pygame, constants, circleshape, player
+import pygame
+from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
-    pb = player.PLAYER(x = constants.SCREEN_WIDTH/2, y = constants.SCREEN_HEIGHT/2)
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        pb.draw(screen)
+
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
     main()
